1d865ef948bc0bce54cc69ef979e878b4b6a034f
masum <mclasix@gmail.com>
2025-04-04 19:48:46 +0700

    feat: implement new screens for Bottom Sheet, Scroll To Hide, and Toast functionalities

--- apps/masumdev/app/index.tsx
+++ apps/masumdev/app/index.tsx
@@ -1,178 +1,84 @@
-import { useBottomSheet } from '@masumdev/bottom-sheet';
-import React, { useRef, useState } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
   TouchableOpacity,
-  Linking,
-  Button,
+  StyleSheet,
+  Dimensions,
 } from 'react-native';
-import Svg, { G, Path } from 'react-native-svg';
-import { useToast } from '@masumdev/rn-toast';
+import { Link } from 'expo-router';
+import {
+  AlignVerticalJustifyEnd,
+  Bell,
+  GalleryHorizontal,
+} from 'lucide-react-native';
 
-export default function Index() {
-  const [whatsNextYCoord, setWhatsNextYCoord] = useState<number>(0);
-  const scrollViewRef = useRef<null | ScrollView>(null);
-  const { expand, setSheetTitle, setContent, close } = useBottomSheet();
-  const { showToast } = useToast();
-
-  const openSheet = () => {
-    setSheetTitle("What's next?");
-    setContent(<Button title="Close" onPress={close} />);
-    expand();
-    showToast('Bottom Sheet Opened', 'success');
-  };
+export default function HomeScreen() {
+  const listLibrary = [
+    {
+      id: 1,
+      title: 'Rn Toast',
+      path: '/toast',
+      icon: <Bell size={30} color={'black'} />,
+    },
+    {
+      id: 2,
+      title: 'Bottom Sheet',
+      path: '/bottom-sheet',
+      icon: <AlignVerticalJustifyEnd size={30} color={'black'} />,
+    },
+    {
+      id: 3,
+      title: 'Rn Scroll To Hide',
+      path: '/scroll-to-hide',
+      icon: <GalleryHorizontal size={30} color={'black'} />,
+    },
+  ];
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView
-        ref={(ref) => {
-          scrollViewRef.current = ref;
-        }}
-        contentInsetAdjustmentBehavior="automatic"
-        style={styles.scrollView}
-      >
-        <View style={styles.section}>
-          <Button title="Open sheet" onPress={openSheet} />
-          <Text style={styles.textLg}>Hello there,</Text>
-          <Text
-            style={[styles.textXL, styles.appTitleText]}
-            testID="heading"
-            role="heading"
-          >
-            Welcome Masumdev 👋
-          </Text>
-        </View>
-        <View style={styles.section}>
-          <View style={styles.hero}>
-            <View style={styles.heroTitle}>
-              <Svg
-                width={32}
-                height={32}
-                stroke="hsla(162, 47%, 50%, 1)"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <Path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
-                />
-              </Svg>
-              <Text style={[styles.textLg, styles.heroTitleText]}>
-                You're up and running
-              </Text>
-            </View>
-            <TouchableOpacity
-              style={styles.whatsNextButton}
-              onPress={() => {
-                scrollViewRef.current?.scrollTo({
-                  x: 0,
-                  y: whatsNextYCoord,
-                });
-              }}
-            >
-              <Text style={[styles.textMd, styles.textCenter]}>
-                What's next?
-              </Text>
+    <View style={styles.container}>
+      <View style={styles.gridContainer}>
+        {listLibrary.map((item) => (
+          <Link href={item.path} key={item.id} asChild>
+            <TouchableOpacity style={styles.gridItem}>
+              {item.icon}
+              <Text style={styles.gridItemText}>{item.title}</Text>
             </TouchableOpacity>
-          </View>
-        </View>
-        <View style={styles.section}>
-          <View style={[styles.shadowBox]}>
-            <Text style={[styles.marginBottomMd, styles.textLg]}>
-              Learning materials
-            </Text>
-            <TouchableOpacity
-              style={[styles.listItem, styles.learning]}
-              onPress={() =>
-                Linking.openURL(
-                  'https://nx.dev/getting-started/intro?utm_source=nx-project'
-                )
-              }
-            >
-              <Text style={styles.textMd}>Documentation</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+          </Link>
+        ))}
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#ffffff',
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#fff',
   },
-  section: {
-    padding: 24,
-  },
-  textXL: {
-    fontSize: 48,
-    fontWeight: '600',
-  },
-  textLg: {
-    fontSize: 32,
-    fontWeight: '600',
-  },
-  textMd: {
-    fontSize: 18,
-  },
-  textCenter: {
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
     textAlign: 'center',
   },
-  appTitleText: {
-    paddingTop: 12,
-    fontWeight: '500',
-  },
-  hero: {
-    borderRadius: 12,
-    backgroundColor: '#143055',
-    padding: 36,
-    marginBottom: 24,
-  },
-  heroTitle: {
-    flex: 1,
+  gridContainer: {
     flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
   },
-  heroTitleText: {
-    color: '#ffffff',
-    marginLeft: 12,
-  },
-  whatsNextButton: {
-    backgroundColor: '#ffffff',
-    paddingVertical: 16,
+  gridItem: {
+    width: Dimensions.get('window').width / 2 - 24,
+    padding: 16,
+    marginBottom: 16,
+    backgroundColor: '#f0f0f0',
     borderRadius: 8,
-    width: '50%',
-    marginTop: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  learning: {
-    marginTop: 12,
-  },
-  listItem: {
-    paddingVertical: 12,
-    paddingHorizontal: 16,
-    borderRadius: 8,
-    backgroundColor: '#f2f3f5',
-  },
-  shadowBox: {
-    backgroundColor: '#ffffff',
-    borderRadius: 24,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    padding: 24,
-  },
-  marginBottomMd: {
-    marginBottom: 24,
+  gridItemText: {
+    marginTop: 8,
+    fontSize: 16,
+    fontWeight: '500',
   },
 });
---
+++ apps/masumdev/app/scroll-to-hide/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function ScrollToHideScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Scroll To Hide Screen</Text>
+    </View>
+  );
+}
