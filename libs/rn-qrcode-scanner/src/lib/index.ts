4a1abc8508cab6946c0999f0bb84dccb90364b66
masum <mclasix@gmail.com>
2025-04-13 22:37:56 +0700

    refactor: remove unused export from index.ts
    The export for 'types/cameraScreenPermission' was removed as it is no longer used in the codebase, reducing unnecessary imports and improving maintainability.

--- libs/rn-qrcode-scanner/src/lib/index.ts
+++ libs/rn-qrcode-scanner/src/lib/index.ts
@@ -1,4 +1,3 @@
 export * from './components/QRCodeScanner';
 export * from './components/CameraScreenPermission';
 export type * from './types';
-export type * from './types/cameraScreenPermission';
